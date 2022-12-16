8c19766082762cc45ed2bd27a642f6a4b6c5e1b8
Mohammed Tamim <alexmerceram@hotmail.com>
2022-12-16 02:01:20 +0100

    Minor adjustments to Window

--- Claudius/Include/Window.h
+++ Claudius/Include/Window.h
@@ -12,11 +12,9 @@ struct Window {
 	using Title = std::string_view;
 
 public:
-	Window(Title title, SDL_WindowFlags flags)
-		:m_Title(title)
-	{
+	Window(Title title, SDL_WindowFlags flags) {
 		m_Window
-			= SDL_CreateWindow(m_Title.data(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_Dimensions.m_Width, m_Dimensions.m_Height, flags);
+			= SDL_CreateWindow(title.data(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, m_Dimensions.m_Width, m_Dimensions.m_Height, flags);
 
 		if (m_Window == nullptr) {
 			throw std::runtime_error::runtime_error(SDL_GetError());
@@ -35,7 +33,6 @@ public:
 	Window& operator=(Window&&) = delete;
 
 public:
-	Title m_Title = "Default";
 	const Dimensions m_Dimensions = { 800, 600 };
 	SDL_Window* m_Window = nullptr;
 };
